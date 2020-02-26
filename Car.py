b16ad080274ffe35e0f400d12b190e2e4377c443
Manuel Velez Martinez <m.martinez@fashionette.de>
2020-02-26 14:46:02 +0100

    Creation of tests

--- Car.py
+++ Car.py
@@ -12,7 +12,11 @@ class Car:
         self.speed += 5
 
     def brake(self):
-        self.speed -= 5
+        if self.speed < 5:
+            self.speed = 0
+        else:
+            self.speed -= 5
+
 
     def step(self):
         self.odometer += self.speed
